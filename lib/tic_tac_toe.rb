38e0d5383c125a53f4003f444c2a53b480ea3b1a
Heath Mast <hrmast@gmail.com>
2020-03-27 17:07:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,9 +64,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) == 4 % 2
+  if turn_count(board) % 2 === 0 
     return "X"
-  else turn_count(board) == 15 % 2
+  else 
     return "O"
   end
 end
